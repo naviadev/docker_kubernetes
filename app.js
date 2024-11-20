0742483604d9a635a3decfbd32e311e447e05bf1
naviadev <qo2oq1020@gmail.com>
2024-11-20 15:57:37 +0900

    NPM | express 설치 및 app.js 선언

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello from Mock PaaS</h1>');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
